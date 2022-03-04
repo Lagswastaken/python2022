7ad7dbdf849c1e2694ae93fccdb19af477bc8098
Lagswastaken <conkni25@kingshigh.school.nz>
2022-03-04 14:55:48 +1300

    yes

--- 6_elif.py
+++ 6_elif.py
@@ -21,11 +21,44 @@ if age >= 18:
 	print("You are considered an adult")
 elif age < 18:
 	print ("You are considered a minor")
-"""
+
 #4
 fav_movie = (input("What is your favourite movie?\n"))
 if fav_movie == "The Batman":
 	print("You have an excelent taste in movies")
 else:
-	print(f"Star Wars is better than {fav_movie}")
+	print(f"The Batman is better than {fav_movie}")
+"""
+"""
 #5
+have_you_heard = (input("Have you ever heard of the Tradegy of Darth Plagieus the Wise?\n")).lower()
+if have_you_heard == "yes":
+	print("Good, Now Execute Order 66")
+else: 
+	print("I thought not It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
+
+"""
+"""	
+#6
+director = (input("How directed the Passion of Christ?\n")).lower()
+if director == "mel gibson":
+	print("That is correct, Mel Gibson did indeed direct the Passion of Christ")
+else:
+	print("No, Mel Gibson actually directed the Passion of Christ")
+
+"""
+#7
+score = 0
+answer_1 = (input("What movie franchise stars Ewan McGregor and Hayden Christensen?\n")).lower()
+if answer_1 == "star wars":
+	score = score + 1
+answer_2 = (input("What Batman movie stars Robert Pattinson?\n")).lower()
+if answer_2 == "the batman":
+	score = score + 1
+answer_3 = (input("Who is the main villain of Star Wars: A New Hope?\n")).lower()
+if answer_3 == "darth vader":
+	score = score + 1
+answer_4 = input("What video game has characters from Marvel, Star Wars and DC in one?\n").lower()
+if answer_4 == "fortnite":
+	score = score + 1
+print(f"Congratulations, you got {score} questions right!")
---
+++ 7_lists.py
@@ -0,0 +1,16 @@
+#1
+fibonacci = ["0", "1", "1", "2", "3", "5", "8", "13", "21", "34" ]
+print (fibonacci)
+#2
+favourite_fruits = ["Bananas", "Strawberries", "Blueberries", "Raspberries", "Mango"]
+print (favourite_fruits)
+#3
+youtubers = ["3C Films", "Sidemen", "Game Theory", "Markiplier"]
+print (youtubers)
+#4
+songs = []
+songs.append ("Baby Shark")
+songs.append ("Your Ex-Lover is Dead")
+songs.append("Creep")
+songs.append("Rape Me")
+print (songs)
