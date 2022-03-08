f08276a1a029fa2a5582c9e9545e486cf0c2f54a
Lagswastaken <conkni25@kingshigh.school.nz>
2022-03-09 10:54:37 +1300

    t

--- 8_loops.py
+++ 8_loops.py
@@ -1,19 +1,23 @@
 print("Hi, welcome to Ice Cream Maker")
 order_complete = False
 toppings_list = []
+topping_count = 0
+toppings_avalible = ["Chocolate Sauce", "Raspberry Sauce", "Caramel Sauce", "Strawberry Sauce", "Sprinkles", "Chocolate Hail", "Nuts", "Flake", "M&ms", "Gummy Bears"]
+print (toppings_avalible)
+while order_complete == False or topping_count <6:
+ 	topping = input("What toppings would you like from the list above? - push enter to finish\n").title()
+ 	if topping =="": 
+ 		print("Order Done")
+ 		order_complete = True
+ 	elif topping not in toppings_avalible:
+ 		print("We don't have that topping in stock")
+ 	else: 
+ 		print("Great, adding it to your ice cream")
+ 		toppings_list.append(topping)
+ 		topping_count+=1
 
-while order_complete == False:
-topping = input("What topping? - push enter to finish")
-if topping == "": 
-print("Order Done")
-order_complete = True
-elif topping in toppings_list: 
-print("You already have that topping")
-else: 
-print("Great, adding it to the list")
-toppings_list.append(topping)
 
 print("Here are your toppings")
 
-print(toppings_list.join(","))
+print(("\n").join(toppings_list))
 
