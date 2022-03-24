067b8e0e164723c558b6fb0c06e8caded5dfe2af
Lagswastaken <conkni25@kingshigh.school.nz>
2022-03-25 09:56:19 +1300

    25/03/2022

--- text_adventure.py
+++ text_adventure.py
@@ -52,26 +52,26 @@ Item.description =""
 torch = Item("a torch", "torch", "flashlight", "a flashlight")
 torch.description = ("An old black flashlight, it doesn't reach far but it's better than nothing")
 
-note_1 = Item("a white note ", "white note")
-note_1.description = ("It's been 6 weeks since the missing children were last seen, celebrating a birthday party and then they went missing.")
+white_note = Item("a white note ", "white note")
+white_note.description = ("It's been 6 weeks since the missing children were last seen, celebrating a birthday party and then they went missing.")
 
-note_2 = Item("a black note", "black note")
-note_2.description = ("Gabriel was the first to be lured away by %$#$%@$ @$%#@%$, the guests that night say they saw her following someone dressed in a Spring Bonnie suit")
+black_note = Item("a black note", "black note")
+black_note.description = ("Gabriel was the first to be lured away by %$#$%@$ @$%#@%$, the guests that night say they saw her following someone dressed in a Spring Bonnie suit")
 
-note_3 = Item("a pink note", "pink note")
-note_3.description = ("Susie was the second to be lured away by Wi^$#%^ Af$#^$, the guests that night say they saw her following someone dressed in a Spring Freddy suit, the guests vividly remember the suits eyes glowing purple.")
+pink_note = Item("a pink note", "pink note")
+pink_note.description = ("Susie was the second to be lured away by Wi^$#%^ Af$#^$, the guests that night say they saw her following someone dressed in a Spring Freddy suit, the guests vividly remember the suits eyes glowing purple.")
 
-note_4 = Item("a green note", "green note")
-note_4.description =("Jeremy was the third to be lured away by Wi&*ll*# Af*&N, the guests that night say they saw him following a security guard into one of the back party rooms. The guests say his skin was purple, like it was rotting")
+green_note = Item("a green note", "green note")
+green_note.description =("Jeremy was the third to be lured away by Wi&*ll*# Af*&N, the guests that night say they saw him following a security guard into one of the back party rooms. The guests say his skin was purple, like it was rotting")
 
-note_5 = Item("a blue note", "blue note")
-note_5.description =("Fritz was the 4th child to be taken away that night by Will$#m Aft#n, guests saw nothing, they say it was like she never even went missing.")
+blue_note = Item("a blue note", "blue note")
+blue_note.description =("Fritz was the 4th child to be taken away that night by Will$#m Aft#n, guests saw nothing, they say it was like she never even went missing.")
 
-note_6 = Item("a red note", "red note")
-note_6.description =("Gabriel was the last to be taken away by Micheal Afton, it was him, Micheal Afton killed these children, MICHEAL AFTON. ITS ME. MICHEAL KILLED THEM")
+red_note = Item("a red note", "red note")
+red_note.description =("Gabriel was the last to be taken away by Micheal Afton, it was him, Micheal Afton killed these children, MICHEAL AFTON. ITS ME. MICHEAL KILLED THEM")
 
-note_7 = Item("a orange note", "orange note")
-note_7.description =("It was William Afton, not Micheal. Micheal is his son, William is trying to blame him for this, listen to me, I know, I was there.")
+orange_note = Item("a orange note", "orange note")
+orange_note.description =("It was William Afton, not Micheal. Micheal is his son, William is trying to blame him for this, listen to me, I know, I was there.")
 
 
 
@@ -136,6 +136,7 @@ def look_at(item):
 @when("take the ITEM")
 @when("pick up the ITEM")
 def pickup(item):
+	global torch_got
 	if item in current_room.items:
 		t = current_room.items.take(item)
 		inventory.add(t)
@@ -146,31 +147,43 @@ def pickup(item):
 			party_room_two.description = "Bonnies empty suit lies on the floor, missing its head, Gabriels body lies lifelessly next to Bonnies empty suit, Gabriels spirit once possessed this suit and has now found peace thanks to you."
 			inventory.take("head")
 			print("A Black note falls out of Bonnies head.")
-			party_room_two.items.add(note_2)
+			black_note_got = True
+			party_room_two.items.add(black_note)
 		elif item in inventory and current_room == party_room_one and t == chicas_head:
 			print("You lift Chicas head off the suit, inside the old spring lock suit you see a slumped over, bloody, rotting, body. You drag the body out of the old, smelly, revolting, animatronic suit and lay it on the ground. The body belongs to one of the missing children, Susie.")
 			party_room_one.description = "Chicas empty suit lies on the floor, missing its head, Susies body lies lifelessly next to Chicas empty suit, Susies spirit once possessed this suit and has now found peace thanks to you."
 			inventory.take("head")
 			print("A Pink note falls out of Chicas head.")
-			party_room_one.items.add(note_3)
+			party_room_one.items.add(pink_note)
+			pink_note_got = True
 		elif item in inventory and current_room == kids_cove and t == foxys_head:
 			print("You lift Foxys head off the suit, inside the old spring lock suit you see a slumped over, bloody, rotting, body. You drag the body out of the old, smelly, revolting, animatronic suit and lay it on the ground. The body belongs to one of the missing children, Jeremy.")
 			kids_cove.description = "Foxys empty suit lies on the floor, missing its head, Jeremys body lies lifelessly next to Foxys empty suit, Jeremys spirit once possessed this suit and has now found peace thanks to you."
 			inventory.take("head")
 			print("A Green note falls out of Foxys head.")
-			kids_cove.items.add(note_4)
+			kids_cove.items.add(green_note)
+			green_note_got = True
 		elif item in inventory and current_room == office and t == freddies_head:
 			print("You lift Freddys head off the suit, inside the old spring lock suit you see a slumped over, bloody, rotting, body. You drag the body out of the old, smelly, revolting, animatronic suit and lay it on the ground. The body belongs to one of the missing children, Fritz.")
 			office.description = "Freddys empty suit lies on the floor, missing its head, Fritzs body lies lifelessly next to Freddys empty suit, Fritzs spirit once possessed this suit and has now found peace thanks to you."
 			inventory.take("head")
 			print("A Blue note falls out of Freddys head.")
-			office.items.add(note_5)
+			office.items.add(blue_note)
+			blue_note_got = True
 		elif item in inventory and current_room == show_stage and t == golden_freddies_head:
 			print("You lift Golden Freddys head off the suit, inside the old spring lock suit you see a slumped over, bloody, rotting, body. You drag the body out of the old, smelly, revolting, animatronic suit and lay it on the ground. The body belongs to one of the missing children, Gabriel.")
 			show_stage.description = "Golden Freddys empty suit lies on the floor, missing its head, Gabriels body lies lifelessly next to Golden Freddys empty suit, Gabriels spirit once possessed this suit and has now found peace thanks to you."
 			inventory.take("head")
 			print("A Red note falls out of Golden Freddys head.")
-			show_stage.items.add(note_6)
+			show_stage.items.add(red_note)
+			red_note_got = True
+		elif item =="white_note":
+			print("You have found the first note.") 
+			white_note_got = True
+		elif item == "torch":
+			print("You have found your torch")
+			torch_got = True
+			parts_and_services.description = "All the old run down animatronic suits sit in the dark and musty room. This room has a haunting vibe to it, like something bad happened here."
 	else:
 		print(f"There is no {item} in this room")
 
@@ -210,20 +223,20 @@ def look():
 
 @when("use ITEM")
 def use(item):
-	if item in inventory and current_room == office and item == "torch":
+	if item in inventory and current_room == parts_and_services and item == "torch":
 		print("You use the torch and in the corner of the room Shadow Bonnie appears, creepily laughing, Shadow Bonnie disappears and on the floor lies a note.")	
-		office.items.add(note_1)
+		parts_and_services.items.add(white_note)
 		print("A white note falls out of the sky.")
 	elif item in inventory and current_room == show_stage and item =="torch":
 		print("You use the torch and in the corner of the room you spot Golden Freddys lifeless animatronic suit twitching slowly in the corner.")
 		show_stage.items.add(golden_freddies_head)
 	elif item in inventory and current_room == game_area and item =="torch":
-		print("You use the torch, In the corner sits a small, crying child, you go up to them and ask if they are alright. You close your eyes and they are gone. A note sits on the floor")
-		game_area.items.add(note_7)
+		print("You use the torch, In the corner sits a small, crying child, you go up to them and ask if they are alright. You close your eyes and they are gone.")
+		game_area.items.add(orange_note)
 		print("An orange note sits where the crying child just was.")
 	elif item in inventory and current_room == kids_cove and item =="torch":
 		print("You use the torch, In the corner sits the same small, crying child you saw earlier, you go up to them, they start talking to you.")
-		print("My name is Evan Afton, the son of William Afton, I'm telling you, HE killed them, he took those children, I was there, HES LYING, IT WAS MICHEAL. YOU CANT RUN FROM THE TRUTH WILLIAM.")	
+		print("My name is Evan Afton, the son of William Afton, I'm telling you, HE killed them, he took those children, I was there, HES LYING, IT WAS MICHEAL. YOU CANT RUN FROM THE TRUTH WILLIAM.")
 	elif item not in inventory:
 		print("You do not have this {item} in your inventory")
 	else:
@@ -238,15 +251,39 @@ def use(item):
 #OBJECTIVES
 ###################
 
+@when("objectives")
+def objectives():
+	print("Objective One, Find your torch. Objective Complete:", torch_got)
+	if torch_got == True:
+		print("Objective Two, Investigate Parts and Services. Objective Complete:", white_note_got)
+	if white_note_got == True:
+		print("Objective Three, Investigate Party Room Two. Objective Complete:", black_note_got)
+	if black_note_got == True:
+		print("Objective Four, Investigate Party Room One. Objective Complete:", pink_note_got)
+	if pink_note_got == True:
+		print("Objective Five, Investigate Kids Cove. Objective Complete:", green_note_got)
+	if green_note_got == True:
+		print("Objective Six, Investigate the Security Office. Objective Complete:", blue_note_got)
+	if blue_note_got == True:
+		print("Objective Seven, Investigate the Show Stage. Objective Complete:", red_note_got)
+	if red_note_got == True:
+		print("Objective Eight, Investigate Games Area. Objective Complete:", green_note_got)
+	if orange_note_got == True:
+		print("Objective Nine, Find the Crying Child. Objective Complete:", crying_child)
+	if crying_child == True:
+		print("Objective Ten, Find the exit. Objective Complete:", exit_found)
+
+
 torch_got = False
-note_1_got = False
-note_2_got = False
-note_3_got = False
-note_4_got = False
-note_5_got = False
-note_6_got = False
-note_7_got = False
-note_8_got = False
+white_note_got = False
+black_note_got = False
+pink_note_got = False
+green_note_got = False
+blue_note_got = False
+red_note_got = False
+orange_note_got = False
+crying_child = False
+exit_found = False
 
 
 ####################
